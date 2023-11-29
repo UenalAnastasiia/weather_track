a1f84d0f3b8ea434916a4e3c3ee41ad07640cc2b
Anastasiia Ünal <velyka.nastya@icloud.com>
2023-11-29 17:58:54 +0100

    add sunset and sunrise

--- src/components/currentWeather_components/CurrentWeather.tsx
+++ src/components/currentWeather_components/CurrentWeather.tsx
@@ -5,6 +5,8 @@ import Card from "react-bootstrap/Card";
 import WeatherImg from "../weather_infos/WeatherImg";
 import WeatherDescription from "../weather_infos/WeatherDescription";
 import CurrentTabs from "./CurrentTabs";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import ThermostatIcon from '@mui/icons-material/Thermostat';
 
 const CurrentWeather = () => {
   const [loading, setLoading] = useState(true);
@@ -41,6 +43,14 @@ const CurrentWeather = () => {
   };
 
 
+  const getTimeFromDate = (timeData: Date) => {
+    let today = new Date(timeData);
+    let timeResult = ("0" + today.getHours()).slice(-2) + ":" + ("0" + today.getMinutes()).slice(-2);
+
+    return timeResult;
+  }
+
+
   const roundTimeQuarterHour = () => {
     let round = 1000 * 60 * 15;
     let today = new Date();
@@ -59,6 +69,10 @@ const CurrentWeather = () => {
         {loading && <p>Loading...</p>}
         {!loading && (
           <Card.Body>
+            <div className="temp-btn">
+              <BottomNavigationAction label="Info" icon={<ThermostatIcon style={{ color: 'white', fontSize: 38 }} />} />
+            </div>
+
             <div>
               {postError && <h1>Es ist ein Fehler aufgetreten: {postError}</h1>}
 
@@ -81,6 +95,18 @@ const CurrentWeather = () => {
                         H:{getRoundTemp(weatherData.daily.temperature_2m_max)}&deg;
                         L:{getRoundTemp(weatherData.daily.temperature_2m_min)}&deg;
                       </h2>
+
+                      <div className="sunDiv">
+                        <span>
+                          <img className="sunImg" src="../assets/sunrise.png" />
+                          <h3>{getTimeFromDate(weatherData.daily.sunrise)}</h3>
+                        </span>
+
+                        <span>
+                          <img className="sunImg" src="../assets/sunset.png" />
+                          <h3>{getTimeFromDate(weatherData.daily.sunset)}</h3>
+                        </span>
+                      </div>
                     </div>
                   </div>
 
